6ef49f650c5b946ad2cf756cc89cd316e8f61591
Simon Wu <siw@microsoft.com>
2017-03-21 15:39:02 +0800

    update copyToPublish

--- CopyToPublish.ps1
+++ CopyToPublish.ps1
@@ -1,6 +1,6 @@
 echo "Bypass Build."
-$copySourceFolder = Join-Path $repositoryRoot "mergedepotdynamic"
-$copyTargetFolder = Join-Path $OutputFolder "mergedepotdynamic"
+$copySourceFolder = Join-Path $repositoryRoot "dotnet-all"
+$copyTargetFolder = Join-Path $OutputFolder "dotnet-all"
 echo "Start copying files and folders from $copySourceFolder to $copyTargetFolder."
 Copy-Item $copySourceFolder $copyTargetFolder -recurse
 echo "Finish copying files and folders from $copySourceFolder to $copyTargetFolder."
